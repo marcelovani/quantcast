0334b824c25ebb5ec0aba1b92f8a91ba5fec424d
Maciej Zgadzaj <maciej.zgadzaj@gmail.com>
2011-10-22 23:44:16 +0200

    General refactoring. Moved Quantcast tag code to external template file, to keep it exactly in line with their requirements

---
+++ theme/quantcast-tag-body.tpl.php
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+  _qevents.push({qacct:"<?php print $vars['pcode']; ?>"});
+//--><!]]>
+</script>
+<noscript>
+  <div style="display:none;"><img src="//pixel.quantserve.com/pixel/<?php print $vars['pcode']; ?>.gif" border="0" height="1" width="1" alt="Quantcast"/></div>
+</noscript>
---
+++ theme/quantcast-tag-full.tpl.php
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+  var _qevents = _qevents || [];
+  (function() {
+    var elem = document.createElement('script');
+    elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+    elem.async = true;
+    elem.type = "text/javascript";
+    var scpt = document.getElementsByTagName('script')[0];
+    scpt.parentNode.insertBefore(elem, scpt);
+  })();
+  _qevents.push({qacct:"<?php print $vars['pcode']; ?>"});
+//--><!]]>
+</script>
+<noscript>
+  <div style="display:none;"><img src="//pixel.quantserve.com/pixel/<?php print $vars['pcode']; ?>.gif" border="0" height="1" width="1" alt="Quantcast"/></div>
+</noscript>
