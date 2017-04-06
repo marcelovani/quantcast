8a8497945fa3cf17ce732d1f3d7fc1ffd71fbbe0
greggles <greggles@36762.no-reply.drupal.org>
2017-04-06 13:42:45 -0600

    Issue #2504149 by greggles: Remove border attribute from pixel

--- theme/quantcast-tag-body.tpl.php
+++ theme/quantcast-tag-body.tpl.php
@@ -4,5 +4,5 @@
 //--><!]]>
 </script>
 <noscript>
-  <div style="display:none;"><img src="//pixel.quantserve.com/pixel/<?php print $vars['quantcast_code_url']; ?>" border="0" height="1" width="1" alt="Quantcast"/></div>
+  <div style="display:none;"><img src="//pixel.quantserve.com/pixel/<?php print $vars['quantcast_code_url']; ?>" height="1" width="1" alt="Quantcast"/></div>
 </noscript>
